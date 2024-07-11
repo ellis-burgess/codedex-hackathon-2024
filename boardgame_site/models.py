79d0915ac010232358416149515aca30f50ee01a
ellis-burgess <ellismburgess@gmail.com>
2024-07-11 22:54:16 +0100

    Add default values to db

--- boardgame_site/models.py
+++ boardgame_site/models.py
@@ -5,12 +5,12 @@ class Event(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   date = db.Column(db.DateTime, nullable=False)
   duration = db.Column(db.DateTime, nullable=False)
-  recurring = db.Column(db.Boolean, nullable=False, default=False)
+  recurring = db.Column(db.Boolean, nullable=False, default=False, server_default='0')
   frequency = db.Column(db.Text)
   end_recurrence = db.Column(db.DateTime)
   title = db.Column(db.Text, nullable=False)
   description = db.Column(db.Text, nullable=False)
-  event_banner = db.Column(db.String(40), nullable=False, default='default.jpg')
+  event_banner = db.Column(db.String(40), nullable=False, default='default.jpg', server_default='default.jpg')
 
   def __repr__(self):
     return_str = [f"Event: "]
