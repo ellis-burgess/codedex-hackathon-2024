6a9df8e1a9b1fccf62bdc2753ca6d443d01f58f4
ellis-burgess <ellismburgess@gmail.com>
2024-07-11 18:38:59 +0100

    Create list of nav urls

--- boardgame_site/__init__.py
+++ boardgame_site/__init__.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my top secret key'
+
+# Pages to include in the nav bar
+def get_navaddresses():
+  with app.app_context():
+    return [
+      {'name': 'Home', 'url': url_for('index')},
+      {'name': 'About', 'url': url_for('about')},
+    ]
+    
+# Context processor to make navaddresses available globally
+@app.context_processor
+def inject_navaddresses():
+  return dict(navaddresses=get_navaddresses())
 
 from boardgame_site import routes
