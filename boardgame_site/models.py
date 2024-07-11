b438ed79ba5fe74cf88a6ee934f6cfbf42059de2
ellis-burgess <ellismburgess@gmail.com>
2024-07-11 23:24:21 +0100

    Refine events table

--- boardgame_site/models.py
+++ boardgame_site/models.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from boardgame_site import db
+from sqlalchemy import text
 
 class Event(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  date = db.Column(db.DateTime, nullable=False)
-  duration = db.Column(db.DateTime, nullable=False)
-  recurring = db.Column(db.Boolean, nullable=False, default=False, server_default='0')
+  start_time = db.Column(db.DateTime, nullable=False)
+  recurring = db.Column(db.Boolean, nullable=False, default=False, server_default=text('0'))
   frequency = db.Column(db.Text)
   end_recurrence = db.Column(db.DateTime)
   title = db.Column(db.Text, nullable=False)
@@ -14,7 +14,7 @@ class Event(db.Model):
 
   def __repr__(self):
     return_str = [f"Event: "]
-    return_str.append(f"'{self.date}'")
+    return_str.append(f"'{self.start_time}'")
     if self.recurring == True:
       return_str.append("(recurring event)")
       return_str.append(f"'{self.frequency}'")
